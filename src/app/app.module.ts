edcf5d171f74daa8880acad261c350f5a754cf76
kiranraj2208 <kiranraj2208@gmail.com>
2019-05-11 15:40:14 +0530

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoTableComponent } from './todo-table/todo-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoTableComponent
   ],
   imports: [
     BrowserModule,
